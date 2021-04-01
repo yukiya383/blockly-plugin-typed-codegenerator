075aa6820cd1b35a4021c117ae8a78b6c7d0520f
yukiya <y181m383@outlook.jp>
2021-04-01 11:08:21 +0900

    add @license

--- python.d.ts
+++ python.d.ts
@@ -1,4 +1,10 @@
 /**
+ * @license
+ * Copyright 2021 yukiya383
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
  * Override declaration of javascript.d.ts
  */
 declare module 'blockly/python' {
